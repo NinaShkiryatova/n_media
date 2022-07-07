f18a0118f5a4b723f4607a8cd80c58a6e9c36a26
NinaShkiryatova <WildForests@yahoo.com>
2022-07-07 11:35:57 +0900

    Добавлен код по д/з Обработка Событий

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,23 @@
+package ru.netology.nmedia.dto
+
+data class Post (
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    var likedByMe: Boolean = false,
+    var likesCount: Int,
+    var sharedCount: Int
+)
+
+fun formatCount (quantity: Int): String {
+    val result: String = when (quantity){
+        in 0..999 -> quantity.toString()
+        in 1000..1099 -> (quantity/1000).toString() + "K"
+        in 1100..9999 -> ((quantity/100)/10.0).toString() + "K"
+        in 10000..999999 -> (quantity/1000).toString() + "K"
+        in 1000000..1099999 -> ((quantity/1000000)).toString() + "M"
+        else -> ((quantity/100000)/10.0).toString() + "M"
+    }
+    return result
+}
