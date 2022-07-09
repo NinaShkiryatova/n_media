b2033367de798ec58f85db6641cd04ea0c1a96d2
NinaShkiryatova <WildForests@yahoo.com>
2022-07-09 19:23:58 +0900

    Внесены правки в код и структуру проекта по д/з MVVM

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,54 +2,43 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.annotation.DrawableRes
 import androidx.core.view.setPadding
+import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.dto.formatCount
+import ru.netology.nmedia.viewModel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post (
-            id = 1,
-            author = "Нетология. Университет интернет-профессий.",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия - помочь встать на путь роста, и начать цепочку перемен → http://netolo.gy//fyb",
-            published = "02 июня в 18:37",
-            likedByMe = false,
-            likesCount = 1111,
-            sharedCount = 999
-        )
-        with(binding) {
-            authorName.text = post.author
-            date.text = post.published
-            postText.text = post.content
-            likesCount.text = post.likesCount.toString()
-            shareCount.text = post.sharedCount.toString()
-            if(post.likedByMe){
-                likes.setImageResource(R.drawable.ic_redlikes_24dp)
-            }
-            likes.setOnClickListener{
-                post.likedByMe = !post.likedByMe
-                if (post.likedByMe) {
-                    likes.setImageResource(R.drawable.ic_redlikes_24dp)
-                    post.likesCount++
-                    likesCount.text = formatCount(post.likesCount)
+        val viewModel: PostViewModel by viewModels()
 
-                }
-                else {
-                    likes.setImageResource(R.drawable.ic_likes_24dp)
-                    post.likesCount--
-                    likesCount.text = formatCount(post.likesCount)
-                }
-            }
-            share.setOnClickListener{
-                post.sharedCount++
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                authorName.text = post.author
+                date.text = post.published
+                postText.text = post.content
+                likesCount.text = formatCount(post.likesCount)
                 shareCount.text = formatCount(post.sharedCount)
+                likes.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_redlikes_24dp else R.drawable.ic_likes_24dp
+                )
             }
+        }
+
+        binding.likes.setOnClickListener {
+            viewModel.like()
+        }
+        binding.share.setOnClickListener{
+            viewModel.share()
         }
     }
 }
